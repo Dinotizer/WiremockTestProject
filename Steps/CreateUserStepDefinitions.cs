62527244638402735ec7965c7b8645d658ea4544
Sheridan Irvine <sheridan.irvine@exclaimer.com>
2024-07-10 10:26:34 +0100

    Working API test without wiremock

--- Steps/CreateUserStepDefinitions.cs
+++ Steps/CreateUserStepDefinitions.cs
@@ -1,33 +1,48 @@
-using System;
+using ReqresAPIClient.Models;
+using ReqresAPIClient;
 using Reqnroll;
+using RestSharp;
+using System.Threading.Tasks;
 
 namespace WiremockTestProject.Steps
 {
     [Binding]
     public class CreateUserStepDefinitions
     {
+        private const string BASE_URL = "https://reqres.in/";
+        private readonly RequestCreateUser createUserReq;
+        private RestResponse response;
+
+        public CreateUserStepDefinitions(RequestCreateUser createUserReq)
+        {
+            this.createUserReq = createUserReq;
+        }
+
         [Given("I input name (.*)")]
         public async Task GivenIInputName(string name)
         {
-            throw new PendingStepException();
+            createUserReq.name = name;
         }
 
         [Given("I input job (.*)")]
         public async Task GivenIInputJob(string job)
         {
-            throw new PendingStepException();
+            createUserReq.job = job;
         }
 
         [When("I send request to create user")]
         public async Task WhenISendRequestToCreateUser()
         {
-            throw new PendingStepException();
+            var api = new Demo();
+            response = await api.CreateNewUser(BASE_URL, createUserReq);
         }
 
         [Then("a user is created")]
         public async Task ThenAUserIsCreated()
         {
-            throw new PendingStepException();
+            var content = HandleContent.GetContent<ResponseCreateUser>(response);
+            Assert.AreEqual(createUserReq.name, content.name);
+            Assert.AreEqual(createUserReq.job, content.job);
         }
     }
 }
