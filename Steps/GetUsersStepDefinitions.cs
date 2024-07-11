e4bb05f6cd3765fd6ad00ff7eb04b81d1d9107c5
Sheridan Irvine <sheridan.irvine@exclaimer.com>
2024-07-11 09:44:32 +0100

    Added json file for mock data

--- Steps/GetUsersStepDefinitions.cs
+++ Steps/GetUsersStepDefinitions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Playwright;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Reqnroll;
 using ReqresAPIClient;
 using ReqresAPIClient.Models;
@@ -14,7 +16,7 @@ namespace WiremockTestProject.Steps
         private const string BASE_URL = "https://reqres.in/";
         private readonly Users users;
         private RestResponse response;
-        //private IRestResponse response;
+        private dynamic mockResponse;
 
         public GetUsersStepDefinitions(Users users)
         {
@@ -22,49 +24,73 @@ namespace WiremockTestProject.Steps
             this.users = users;
         }
 
-        /*
-        Without Wiremock
-        */
-        //[Given("I send a Get request to the List Users API")]
-        //public async Task GivenISendAGetRequestToTheListUsersAPI()
-        //{
-        //    var api = new Demo();
-        //    response = await api.GetUsers(BASE_URL, users);
-        //}
+        /// <summary>
+        ///  Without Wiremock
+        /// </summary>
+        [Given("I send a Get request to the List Users API")]
+        public async Task GivenISendAGetRequestToTheListUsersAPI()
+        {
+            var api = new Demo();
+            response = await api.GetUsers(BASE_URL, users);
+        }
 
-        //[Then("the API returns requested user details")]
-        //public async Task ThenTheAPIReturnsRequestedUserDetails()
-        //{
-        //    var content = HandleContent.GetContent<Users>(response);
-        //    Assertions.ReferenceEquals(response, content);
+        [Then("the API returns requested user details")]
+        public async Task ThenTheAPIReturnsRequestedUserDetails()
+        {
+            var content = HandleContent.GetContent<Users>(response);
+            Assertions.ReferenceEquals(response, content);
 
-        //    //Assert.AreEqual(2, content.page);
-        //}
+            //Assert.AreEqual(2, content.page);
+        }
 
-
-        /*
-        with Wiremock
-        */
+        /// <summary>
+        ///  With Wiremock
+        /// </summary>
         [Given("the mock server is running")]
         public async Task GivenTheMockServerIsRunning()
         {
-            server.Given(Request.Create().WithPath("/api/users/2").UsingGet())
-              .RespondWith(Response.Create()
-              .WithStatusCode(200)
-              .WithHeader("Content-Type", "application/json")
-              .WithBody(@"{
-                  ""data"": {
-                      ""id"": 2,
-                      ""email"": ""janet.weaver@reqres.in"",
-                      ""first_name"": ""Janet"",
-                      ""last_name"": ""Weaver"",
-                      ""avatar"": ""https://reqres.in/img/faces/2-image.jpg""
-                  },
-                  ""support"": {
-                      ""url"": ""https://reqres.in/#support-heading"",
-                      ""text"": ""To keep ReqRes free, contributions towards server costs are appreciated!""
-                  }
-              }"));
+            // Refer to the file path relative to the output directory
+            string mockResponseJson = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "MockData", "GetUsersMockResponse.json"));
+            mockResponse = JsonConvert.DeserializeObject<dynamic>(mockResponseJson);
+
+            // Extract data from the JSON object
+            string path = mockResponse.path;
+            string method = mockResponse.method;
+            int statusCode = mockResponse.status;
+            var headers = mockResponse.headers.ToObject<Dictionary<string, string>>();
+            string body = JsonConvert.SerializeObject(mockResponse.body);
+
+            // Set up a mock response for the specified request
+            var requestBuilder = Request.Create().WithPath(path).UsingMethod(method);
+            var responseBuilder = Response.Create().WithStatusCode(statusCode).WithBody(body);
+
+            foreach (var header in headers)
+            {
+                responseBuilder.WithHeader(header.Key, header.Value);
+            }
+
+            server.Given(requestBuilder).RespondWith(responseBuilder);
+
+            /// <summary>
+            ///  You can add the content in the step like this, but it isn;t as maintainable. The above does the same but the below content is in a mock data json file instead.
+            /// </summary>
+            //server.Given(Request.Create().WithPath("/api/users/2").UsingGet())
+            //  .RespondWith(Response.Create()
+            //  .WithStatusCode(200)
+            //  .WithHeader("Content-Type", "application/json")
+            //  .WithBody(@"{
+            //      ""data"": {
+            //          ""id"": 2,
+            //          ""email"": ""janet.weaver@reqres.in"",
+            //          ""first_name"": ""Janet"",
+            //          ""last_name"": ""Weaver"",
+            //          ""avatar"": ""https://reqres.in/img/faces/2-image.jpg""
+            //      },
+            //      ""support"": {
+            //          ""url"": ""https://reqres.in/#support-heading"",
+            //          ""text"": ""To keep ReqRes free, contributions towards server costs are appreciated!""
+            //      }
+            //  }"));
         }
 
         [Given("I send a Get request to the List Users API using Wiremock")]
@@ -78,22 +104,33 @@ namespace WiremockTestProject.Steps
         [Then("the API returns requested user details using Wiremock")]
         public async Task ThenTheAPIReturnsRequestedUserDetailsUsingWiremock()
         {
-            Assert.AreEqual(200, (int)response.StatusCode);
-            var expectedBody = @"{
-                  ""data"": {
-                      ""id"": 2,
-                      ""email"": ""janet.weaver@reqres.in"",
-                      ""first_name"": ""Janet"",
-                      ""last_name"": ""Weaver"",
-                      ""avatar"": ""https://reqres.in/img/faces/2-image.jpg""
-                  },
-                  ""support"": {
-                      ""url"": ""https://reqres.in/#support-heading"",
-                      ""text"": ""To keep ReqRes free, contributions towards server costs are appreciated!""
-                  }
-              }";
-            //Assert.AreEqual(expectedBody, response.Content);
-            Assertions.ReferenceEquals(expectedBody,response.Content);
+            Assert.AreEqual((int)mockResponse.status, (int)response.StatusCode);
+
+            // Parse JSON objects to compare them
+            var expectedJson = JObject.Parse(JsonConvert.SerializeObject(mockResponse.body));
+            var actualJson = JObject.Parse(response.Content);
+
+            Assert.AreEqual(expectedJson.ToString(), actualJson.ToString());
+
+            /// <summary>
+            ///  You can add the content in the step like this, but it isn;t as maintainable. The above does the same but the below content is in a mock data json file instead.
+            /// </summary>
+            //Assert.AreEqual(200, (int)response.StatusCode);
+            //var expectedBody = @"{
+            //      ""data"": {
+            //          ""id"": 2,
+            //          ""email"": ""janet.weaver@reqres.in"",
+            //          ""first_name"": ""Janet"",
+            //          ""last_name"": ""Weaver"",
+            //          ""avatar"": ""https://reqres.in/img/faces/2-image.jpg""
+            //      },
+            //      ""support"": {
+            //          ""url"": ""https://reqres.in/#support-heading"",
+            //          ""text"": ""To keep ReqRes free, contributions towards server costs are appreciated!""
+            //      }
+            //  }";
+            ////Assert.AreEqual(expectedBody, response.Content);
+            //Assertions.ReferenceEquals(expectedBody,response.Content);
         }
 
         ~GetUsersStepDefinitions()
